7bbc8c4f47b8e331bf01010810740eabaaf6a9c6
Sindhusaranti <sindhusaranti369@gmail.com>
2025-12-31 18:54:57 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+
+public class Add {
+    public static void main(String a[])
+    {    
+        int num1=14;
+        int num2=1;
+        int result=num1+num2;
+        System.out.println(result);
+        byte by=11;
+        int b=257;
+        by=(byte)b;
+        System.out.println(by);
+        float f=14.25f;
+        int in=(int)f;
+        System.out.println(in);
+    }
+}
